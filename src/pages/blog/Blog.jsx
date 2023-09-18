d0729bd6afdd261a4940ec3aa00249d81df8b7e7
Ramanwork24 <sainiraman8700@gmail.com>
2023-09-18 10:53:26 +0530

    blog create

---
+++ src/pages/blog/Blog.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './blog.css';
+
+function Blog() {
+  return (
+    <div>
+      <div className="blog-page">
+        <h1>This is blog page</h1>
+      </div>
+
+    </div>
+  )
+}
+
+export default Blog;
